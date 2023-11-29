135abf8873917416281dcbbb4496a376bc485f9c
Kanav Bengani <kbengani21@gmail.com>
2023-11-29 11:12:11 -0500

    Changed medStaff to have servEDate .

--- sql_mock_data/Mockaroo_admin.sql
+++ sql_mock_data/Mockaroo_admin.sql
@@ -4,6 +4,15 @@ CREATE TABLE `admin` (
   `password` varchar(30) NOT NULL,
   PRIMARY KEY (`username`)
 );
-insert into admin (username, password) values ('mhinks0', 'pX9=|oN|aE_$');
-insert into admin (username, password) values ('cgozard1', 'fK8)|{');
-insert into admin (username, password) values ('sfylan2', 'mL7u0=bfb');
+
+insert into admin (username, password) values ('brottcher0', 'wN7x9Kbk@M');
+insert into admin (username, password) values ('ecaesar1', 'DSFIH97yUh(&');
+insert into admin (username, password) values ('mmileham2', '&^*&7yiuH');
+insert into admin (username, password) values ('adudhill3', '^*&uyh6');
+insert into admin (username, password) values ('cballard4', 'HUYHJY86y8yh8y*');
+insert into admin (username, password) values ('mditch5', '%^7987yuhuG');
+insert into admin (username, password) values ('blandeaux6', '(*&^&8uy78Yh');
+insert into admin (username, password) values ('mcreber7', 'KHg7&78*');
+insert into admin (username, password) values ('nprangle8', 'G&*uh78&*');
+insert into admin (username, password) values ('bdowley9', 'IUHIg7gh*&^');
+
--- sql_mock_data/Mockaroo_medStaff.sql
+++ sql_mock_data/Mockaroo_medStaff.sql
@@ -6,7 +6,8 @@ CREATE TABLE `medStaff` (
   `last_name` varchar(30) NOT NULL,
   `sex` enum('Male','Female','Other') NOT NULL,
   `dob` date NOT NULL,
-  `servSdate` date NOT NULL,
+  `servSDate` date NOT NULL,
+  `servEDate` date NOT NULL,
   `addrId` int DEFAULT NULL,
   `supervisorId` int DEFAULT NULL,
   `salary` int DEFAULT NULL,
@@ -15,8 +16,13 @@ CREATE TABLE `medStaff` (
   CONSTRAINT `FK_MedSupervisor` FOREIGN KEY (`supervisorId`) REFERENCES `supervisor` (`supervisorId`) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
 
-insert into medStaff (staffId, SSN, first_name, last_name, sex, dob, servSDate, addrId, supervisorId, salary) values (1, '265-50-6547', 'Carin', 'Bonefant', 'Female', '1953-11-18', '2009-08-04', 1, 2, 60000);
-insert into medStaff (staffId, SSN, first_name, last_name, sex, dob, servSDate, addrId, supervisorId, salary) values (2, '258-04-9565', 'Murdock', 'Opfer', 'Female', '1961-12-05', '2013-05-20', 2, 1, 60000);
-insert into medStaff (staffId, SSN, first_name, last_name, sex, dob, servSDate, addrId, supervisorId, salary) values (3, '431-67-8674', 'Cordell', 'Galsworthy', 'Male', '1927-02-22', '2012-05-23', 3, 2, 70000);
-insert into medStaff (staffId, SSN, first_name, last_name, sex, dob, servSDate, addrId, supervisorId, salary) values (4, '800-94-5975', 'Rachele', 'Pessolt', 'Other', '1948-04-08', '2015-02-21', 4, 2, 60000);
-insert into medStaff (staffId, SSN, first_name, last_name, sex, dob, servSDate, addrId, supervisorId, salary) values (5, '717-13-2991', 'Ree', 'Gland', 'Female', '1976-03-06', '2009-07-08', 5, 3, 55000);
+insert into medStaff (staffId, SSN, first_name, last_name, sex, dob, servSDate, serveDate, addrId, supervisorId, salary) values (1, '292-02-3095', 'Drusilla', 'Olner', 'Other', '1934-07-10', '2019-06-30', '2025-02-17', 1, 1, 65000);
+insert into medStaff (staffId, SSN, first_name, last_name, sex, dob, servSDate, serveDate, addrId, supervisorId, salary) values (2, '731-37-8644', 'Loralyn', 'Shakelady', 'Female', '1971-08-09', '2019-10-15', '2024-02-21', 2, 2, 70000);
+insert into medStaff (staffId, SSN, first_name, last_name, sex, dob, servSDate, serveDate, addrId, supervisorId, salary) values (3, '600-31-8447', 'Ric', 'Rozzier', 'Male', '1960-10-20', '2012-02-04', '2027-05-23', 3, 2, 65000);
+insert into medStaff (staffId, SSN, first_name, last_name, sex, dob, servSDate, serveDate, addrId, supervisorId, salary) values (4, '782-17-4709', 'Cordy', 'Monketon', 'Other', '1945-10-27', '2012-03-05', '2025-03-28', 4, 3, 70000);
+insert into medStaff (staffId, SSN, first_name, last_name, sex, dob, servSDate, serveDate, addrId, supervisorId, salary) values (5, '137-40-4088', 'Eustacia', 'Eastwell', 'Male', '1977-02-05', '2019-08-04', '2027-05-08', 5, 1, 50000);
+insert into medStaff (staffId, SSN, first_name, last_name, sex, dob, servSDate, serveDate, addrId, supervisorId, salary) values (6, '212-90-0245', 'Myrilla', 'Derr', 'Male', '1967-09-03', '2012-06-06', '2027-07-07', 6, 1, 60000);
+insert into medStaff (staffId, SSN, first_name, last_name, sex, dob, servSDate, serveDate, addrId, supervisorId, salary) values (7, '870-96-5616', 'Annadiana', 'Poytres', 'Other', '1983-01-30', '2007-03-26', '2025-03-31', 7, 3, 70000);
+insert into medStaff (staffId, SSN, first_name, last_name, sex, dob, servSDate, serveDate, addrId, supervisorId, salary) values (8, '156-05-3494', 'Ula', 'Rusted', 'Female', '1952-05-12', '2019-01-25', '2029-06-26', 8, 3, 50000);
+insert into medStaff (staffId, SSN, first_name, last_name, sex, dob, servSDate, serveDate, addrId, supervisorId, salary) values (9, '655-52-2032', 'Candida', 'Taborre', 'Female', '1943-03-28', '2010-12-10', '2024-11-04', 9, 1, 65000);
+insert into medStaff (staffId, SSN, first_name, last_name, sex, dob, servSDate, serveDate, addrId, supervisorId, salary) values (10, '787-25-8310', 'Shirlene', 'Pilkington', 'Male', '1954-02-06', '2015-05-26', '2027-01-24', 10, 3, 70000);
