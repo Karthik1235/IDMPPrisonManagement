e87e797a1523c84326b3573af4cbb1f7fab162d6
Kanav Bengani <kbengani21@gmail.com>
2023-11-29 11:25:21 -0500

    Made labor have 10 rows.

--- sql_mock_data/Mockaroo_labor.sql
+++ sql_mock_data/Mockaroo_labor.sql
@@ -5,8 +5,13 @@ CREATE TABLE `labor` (
   PRIMARY KEY (`laborId`)
 );
 
-insert into labor (laborId, laborName) values (1, 'gardening');
-insert into labor (laborId, laborName) values (2, 'cleaning');
-insert into labor (laborId, laborName) values (3, 'construction');
-insert into labor (laborId, laborName) values (4, 'carpentry');
-insert into labor (laborId, laborName) values (5, 'plumbing');
+insert into labor (laborId, laborName) values (1, 'janitorial work');
+insert into labor (laborId, laborName) values (2, 'grounds maintenance');
+insert into labor (laborId, laborName) values (3, 'laundry services');
+insert into labor (laborId, laborName) values (4, 'food preparation');
+insert into labor (laborId, laborName) values (5, 'assembly line work');
+insert into labor (laborId, laborName) values (6, 'carpentry');
+insert into labor (laborId, laborName) values (7, 'plumbing');
+insert into labor (laborId, laborName) values (8, 'electrical work');
+insert into labor (laborId, laborName) values (9, 'data entry');
+insert into labor (laborId, laborName) values (10, 'gardening');
